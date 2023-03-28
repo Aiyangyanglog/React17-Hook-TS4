2615c41a1282bc848e27c301f29f195f2f119ff3
ayy <752249120@qq.com>
2023-03-28 15:10:14 +0800

    feat: add typeScript

--- jira/src/screens/project-list/index.tsx
+++ jira/src/screens/project-list/index.tsx
@@ -1,17 +1,20 @@
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useState, useEffect } from "react";
-import { cleanObject, useMount, useDebounce } from "utils";
+import { cleanObject, useMount, useDebounce } from "../../utils";
 import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
+
+// 使用 JS 的同学，大部分的错误都是在runtime(运行时)的时候才发现的
+// 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
-  const debouncedParam = useDebounce(param, 2000);
+  const debouncedParam = useDebounce(param, 200);
   const [list, setList] = useState([]);
 
   useEffect(() => {
