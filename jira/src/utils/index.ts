e3f4e0a5ce9944085c79464a5f72550659c5ea8c
ayy <752249120@qq.com>
2023-03-28 15:35:09 +0800

    feat: add unknown

--- jira/src/utils/index.ts
+++ jira/src/utils/index.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object: object) => {
   //   Object.assign({}, object);
@@ -21,7 +21,8 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+// 后面用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     // 每次在value变化以后，设置一个定时器
