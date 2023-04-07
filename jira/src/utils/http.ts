c6b493a3c38118e513c23feb11796d2bb500ae10
ayy <752249120@qq.com>
2023-04-07 15:45:32 +0800

    feat: add type interface Partial Omit Pick Exclude

--- jira/src/utils/http.ts
+++ jira/src/utils/http.ts
@@ -44,11 +44,49 @@ export const http = async (
       }
     });
 };
+// JS 中的typeof，是在runtime时运行的
+// return typeof 1 === 'number'
 
+// TS 中的typeof，是在静态环境运行的
+// return (...[endpoint, config]: Parameters<typeof http>) =>
 export const useHttp = () => {
   const { user } = useAuth();
-  // TODO 讲解 TS 操作符
-  return (...[endpoint, config]: Parameters<typeof http>) => {
+  // utility type 的用法： 用泛型给它传入一个其他类型，然后 utility type 对这个类型进行某种操作
+  return (...[endpoint, config]: Parameters<typeof http>) =>
     http(endpoint, { ...config, token: user?.token });
-  };
+};
+
+// 联合类型
+let myFavoriteNumber: string | number;
+myFavoriteNumber = "seven";
+myFavoriteNumber = 7;
+// 不能将类型“{}”分配给类型“string | number”。
+// myFavoriteNumber = {};
+let jackFavoriteNumber: string | number;
+
+// 类型别名在很多情况下可以和interface互换
+// interface Person {
+//   name: string;
+// }
+// type Person = { name: string };
+// const xiaoMing: Person = { name: "xiaoming" };
+
+// 类型别名, interface 在这种情况下没法替代type
+type FavoriteNumber = string | number;
+let roseFavoriteNumber: FavoriteNumber = "6";
+
+// interface 也没法实现 Utility Type
+type Person = {
+  name: string;
+  age: number;
+};
+const xiaoMing: Partial<Person> = {};
+const shenMiRen: Omit<Person, "name" | "age"> = {};
+type PersonKeys = keyof Person;
+type PersonOnlyName = Pick<Person, "name" | "age">;
+type Age = Exclude<PersonKeys, "name">;
+
+// Partial 的实现
+type Partial<T> = {
+  [P in keyof T]?: T[P];
 };
