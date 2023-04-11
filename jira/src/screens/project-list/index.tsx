aeef1532d3c64c7cbb19e2213c1560e6f6abef8a
ayy <752249120@qq.com>
2023-04-11 18:05:14 +0800

    fix: optimize list page

--- jira/src/screens/project-list/index.tsx
+++ jira/src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import { List } from "./list";
 import { useState, useEffect } from "react";
 import { cleanObject, useMount, useDebounce } from "../../utils";
 import { useHttp } from "utils/http";
+import styled from "@emotion/styled";
 
 // 使用 JS 的同学，大部分的错误都是在runtime(运行时)的时候才发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
@@ -24,9 +25,14 @@ export const ProjectListScreen = () => {
     client("users").then(setUsers);
   });
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
