7b32eaf0630ec6adba7c528bc13ee70c182b78be
ayy <752249120@qq.com>
2023-04-12 17:06:34 +0800

    feat: add useAsync and useUser,useProject

--- jira/src/screens/project-list/index.tsx
+++ jira/src/screens/project-list/index.tsx
@@ -1,49 +1,31 @@
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { useState, useEffect } from "react";
-import { cleanObject, useMount, useDebounce } from "../../utils";
-import { useHttp } from "utils/http";
+import { useState } from "react";
+import { useDebounce } from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
+import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
 
 // 使用 JS 的同学，大部分的错误都是在runtime(运行时)的时候才发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<null | Error>(null);
-
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
   const debouncedParam = useDebounce(param, 200);
-  const [list, setList] = useState([]);
-  const client = useHttp();
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
 
-  useEffect(() => {
-    setIsLoading(true);
-    client("projects", { data: cleanObject(debouncedParam) })
-      .then(setList)
-      .catch((error) => {
-        setList([]);
-        setError(error);
-      })
-      .finally(() => setIsLoading(false));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam]);
-
-  useMount(() => {
-    client("users").then(setUsers);
-  });
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users} param={param} setParam={setParam} />
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
       {error ? (
         <Typography.Text type={"danger"}>{error.message}</Typography.Text>
       ) : null}
-      <List loading={isLoading} users={users} dataSource={list} />
+      <List loading={isLoading} users={users || []} dataSource={list || []} />
     </Container>
   );
 };
